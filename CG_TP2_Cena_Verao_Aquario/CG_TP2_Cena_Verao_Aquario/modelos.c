cf34848eb258624862105e5e6a058021b523d2d9
FCabeleira <filipe.cabeleira@student.uc.pt>
2017-05-31 15:04:41 +0100

    Melhoramento da função que lê os ficheiros OBJ
    Agora normaliza a dimensão dos objetos e gera as normais às faces e nos vértices.

--- CG_TP2_Cena_Verao_Aquario/CG_TP2_Cena_Verao_Aquario/modelos.c
+++ CG_TP2_Cena_Verao_Aquario/CG_TP2_Cena_Verao_Aquario/modelos.c
@@ -27,11 +27,42 @@
 void importar_modelos(void)
 {
 	goldfish = glmReadOBJ("models/goldfish/GOLDFISH.OBJ");
+	glmUnitize(goldfish);
+	glmFacetNormals(goldfish);
+	glmVertexNormals(goldfish, 90.0);
+
 	manatee = glmReadOBJ("models/manatee/MANATEE1.OBJ");
+	glmUnitize(manatee);
+	glmFacetNormals(manatee);
+	glmVertexNormals(manatee, 90.0);
+
 	orca = glmReadOBJ("models/orca/ORCA.OBJ");
+	glmUnitize(goldfish);
+	glmFacetNormals(goldfish);
+	glmVertexNormals(goldfish, 90.0);
+
 	salmon = glmReadOBJ("models/salmon/SALMON.OBJ");
+	glmUnitize(salmon);
+	glmFacetNormals(salmon);
+	glmVertexNormals(salmon, 90.0);
+
 	shark = glmReadOBJ("models/shark/Shark.obj");
+	glmUnitize(shark);
+	glmFacetNormals(shark);
+	glmVertexNormals(shark, 90.0);
+
 	tiger_shark = glmReadOBJ("models/tiger_shark/TGRSHARK.OBJ");
+	glmUnitize(tiger_shark);
+	glmFacetNormals(tiger_shark);
+	glmVertexNormals(tiger_shark, 90.0);
+
 	trout = glmReadOBJ("models/trout/RB_TROUT.OBJ");
+	glmUnitize(trout);
+	glmFacetNormals(trout);
+	glmVertexNormals(trout, 90.0);
+
 	tuna = glmReadOBJ("models/tuna/TUNA.OBJ");
+	glmUnitize(tuna);
+	glmFacetNormals(tuna);
+	glmVertexNormals(tuna, 90.0);
 }
